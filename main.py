c14ce31e103eb91680146b787b1a1373120f5c6e
Zach Martin <zach@zach-martin.com>
2025-07-09 08:02:40 -0400

    Finished printing constants

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID
 
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
