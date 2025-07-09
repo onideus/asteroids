b7bdb934bdaf6660b64ec0273481272ef673ba7b
Zach Martin <zach@zach-martin.com>
2025-07-09 08:15:14 -0400

    creates window and fills it with black

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import pygame
+from pygame import display
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, \
     ASTEROID_MAX_RADIUS
 
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    # Begin game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0, 0, 0))
+        display.flip()
 
 
 if __name__ == "__main__":
